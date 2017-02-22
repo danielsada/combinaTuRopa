cc6abf8ee9c89a4ab6864db4e12e62efdea30e59
andrescalvavalencia <jerome6852@hotmail.com>
2017-02-21 18:37:29 -0600

    prueba
    prueba

--- combinaTuRopa/combinaTuRopa/ViewController.swift
+++ combinaTuRopa/combinaTuRopa/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
- //Pruebas
+ //Pruebas A
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
