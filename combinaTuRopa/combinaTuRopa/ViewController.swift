155631681377a1cf7ba72e94d3433e444340d452
AndresCal <jerome6852@hotmail.com>
2017-03-21 00:17:14 -0600

    Animacion Botones
    Animacion en la segunda Pantalla y contraints actualizados

--- combinaTuRopa/combinaTuRopa/ViewController.swift
+++ combinaTuRopa/combinaTuRopa/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
- //Pruebas A
+ //Boton 
+    
+    @IBAction func bounce(_ sender: Any) {
+        let button = sender as! UIButton
+        let bounds = button.bounds
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 30, options: .curveEaseInOut, animations: {
+            button.bounds = CGRect(x: bounds.origin.x - 20, y: bounds.origin.y, width : bounds.size.width + 60, height: bounds.size.height)
+        }) { (success:Bool) in
+            if success == true {
+                button.bounds = bounds
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ combinaTuRopa/combinaTuRopa/ViewControllerSeleccionHM.swift
@@ -0,0 +1,51 @@
+//
+//  ViewControllerSeleccionHM.swift
+//  combinaTuRopa
+//
+//  Created by Familia on 20/03/17.
+//  Copyright © 2017 EquipoRMRoman4. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerSeleccionHM: UIViewController{
+    //Boton Mujer
+    @IBAction func botonMujer(_ sender: Any) {
+        let button = sender as! UIButton
+        let bounds = button.bounds
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 30, options: .curveEaseInOut, animations: {
+            button.bounds = CGRect(x: bounds.origin.x - 10, y: bounds.origin.y, width : bounds.size.width + 60, height: bounds.size.height + 15)
+        }) { (success:Bool) in
+            if success == true {
+                UIView.animate(withDuration: 0.5, animations: {
+                    button.bounds = bounds
+                })
+                
+            }
+        }
+
+    }
+    
+    //Boton Hombre
+    @IBAction func botonHombre(_ sender: Any) {
+        let button = sender as! UIButton
+        let bounds = button.bounds
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 30, options: .curveEaseInOut, animations: {
+            button.bounds = CGRect(x: bounds.origin.x + 10, y: bounds.origin.y, width : bounds.size.width + 60, height: bounds.size.height + 15)
+        }) { (success:Bool) in
+            if success == true {
+                UIView.animate(withDuration: 0.5, animations: { 
+                    button.bounds = bounds
+                })
+                
+            }
+        }
+
+    }
+    
+    
+    
+}
