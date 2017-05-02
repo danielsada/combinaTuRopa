1808580cb18d6b440766018b431bc81a6328d53b
Enocruz <b-andon@hotmail.com>
2017-05-01 22:13:52 -0500

    Correcciones
    Correcciones de errores en el mapa con los pies

--- combinaTuRopa/combinaTuRopa/ViewControllerMapa.swift
+++ combinaTuRopa/combinaTuRopa/ViewControllerMapa.swift
@@ -54,7 +54,7 @@ class ViewControllerMapa: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrTiendas.count
+        return 0
     }
     
     /*func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
@@ -63,9 +63,9 @@ class ViewControllerMapa: UIViewController, UITableViewDataSource, UITableViewDe
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celda = tableView.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
-        celda.textLabel?.text = arrTiendas[indexPath.row]
-        celda.detailTextLabel?.font = UIFont.italicSystemFont(ofSize: 11.0)
-        celda.detailTextLabel?.text = arrDirecciones[indexPath.row]
+        //celda.textLabel?.text = arrTiendas[indexPath.row]
+        //celda.detailTextLabel?.font = UIFont.italicSystemFont(ofSize: 11.0)
+        //celda.detailTextLabel?.text = arrDirecciones[indexPath.row]
         return celda
     }
     
